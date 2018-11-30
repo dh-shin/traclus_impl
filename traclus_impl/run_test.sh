5d8cfaba1064bbe29018b54818d751b7186cde3f
DongHwa Shin <dhshin@hcil.snu.ac.kr>
2018-11-30 20:14:31 +0900

    modify run_test.sh

--- traclus_impl/run_test.sh
+++ traclus_impl/run_test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 file_path="../../traclus_data"
 prefix="gwanak_trip_traclus"
-i_file=$file_path"/"$prefix"_input.txt"
-o_file=$file_path"/"$prefix"_output.txt"
-c_file=$file_path"/"$prefix"_cluster.txt"
-p_file=$file_path"/"$prefix"_partitioned.txt"
+i_file=$file_path"/"$prefix"_input.json"
+o_file=$file_path"/"$prefix"_output.json"
+c_file=$file_path"/"$prefix"_cluster.json"
+p_file=$file_path"/"$prefix"_partitioned.json"
 
 python main.py -i $i_file -o $o_file -p $p_file -c $c_file
